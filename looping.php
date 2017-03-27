17e7b5404cf8ae96cbdf3dbb87eb07a5a73a76f1
Ferdowsul Islam Joyon <joyon47@gmail.com>
2017-03-27 16:51:58 +0600

    php loop
    working with loop

---
+++ looping.php
@@ -0,0 +1,12 @@
+<?php  
+$star = array("*", "**", "***", "**", "*"); 
+
+foreach ($star as $value) {
+  echo "$value <br>";
+}
+
+$number = array("1-", "2-", "3-", "4-", "5");
+foreach ($number as $value) {
+  echo $value;
+}
+?>
